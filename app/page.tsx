1268f4a8f5930e9db9a33cc0ac38d7447c29e976
rala-rationallife <s.tsuduki26@gmail.com>
2024-04-20 22:20:47 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import styles from "@styles/page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>CUBE</h1>
+
+      <p>アウトプットしていくサイト</p>
+    </main>
+  );
+}
